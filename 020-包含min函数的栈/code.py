41fb5737b946dd48ae340a33d5fdb61cef40ec27
Zhou Junzuo <joezhow777@gmail.com>
2019-01-20 22:49:24 +0800

    包含min的栈

---
+++ 020-包含min函数的栈/code.py
@@ -0,0 +1,27 @@
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, node):
+        # write code here
+        self.stack.append(node)
+        if not self.min_stack:
+            self.min_stack.append(node)
+        else:
+            if self.min_stack[-1] < node:
+                self.min_stack.append(self.min_stack[-1])
+            else:
+                self.min_stack.append(node)
+    def pop(self):
+        # write code here
+        self.stack.pop(-1)
+        self.min_stack.pop(-1)
+    
+    def top(self):
+        # write code here
+        return self.stack.pop(-1)
+
+    def min(self):
+        # write code here
+        return self.min_stack[-1]
