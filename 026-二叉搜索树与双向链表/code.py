f2e7f85fdcba8aa1277e85a5bec50760bd469625
Zhou Junzuo <joezhow777@gmail.com>
2019-01-21 22:50:48 +0800

    重点题，二叉搜索树转双向链表

---
+++ 026-二叉搜索树与双向链表/code.py
@@ -0,0 +1,24 @@
+
+def Convert(pRootOfTree):
+    if not pRootOfTree: return None
+    stack = []
+    p = pRootOfTree
+    new_h = None
+    pre = None
+    while p or stack:
+        while p:
+            stack.append(p)
+            p = p.left
+        p = stack.pop()
+        if not pre:
+            pre = p
+            new_h = p
+        else:
+            pre.right = p
+            p.left = pre
+            pre = p
+
+        p = p.right
+    return new_h
+
+
