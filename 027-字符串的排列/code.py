9aa63e8f373d27020b1effe47590ccd6f8a57c2d
Zhou Junzuo <joezhow777@gmail.com>
2019-01-22 17:25:00 +0800

    字符串全排列

---
+++ 027-字符串的排列/code.py
@@ -0,0 +1,19 @@
+
+def helper(s):
+    if len(s) == 1:
+        return s[0]
+    res = []
+    for i in range(len(s)):
+        l = helper(s[:i] + s[i+1:])
+        for j in l:
+            res.append(s[i] + j)
+
+    return res
+
+def Permutation(ss):
+    # write code here
+    if not ss: return []
+    words = list(ss)
+    return list(sorted(set(helper(words))))
+
+print(Permutation('aa'))
