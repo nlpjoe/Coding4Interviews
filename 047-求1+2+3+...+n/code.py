ffc2e79502e3c46f3871c1edef5c851bd8e2d8f3
Zhou Junzuo <joezhow777@gmail.com>
2019-01-28 11:57:24 +0800

    抛出异常求sum(n)

---
+++ 047-求1+2+3+...+n/code.py
@@ -0,0 +1,15 @@
+def sum(n):
+    try:
+        1 % n
+        return n + sum(n-1)
+    except:
+        return 0
+
+
+
+class Solution:
+    def Sum_Solution(self, n):
+        return sum(n)
+    
+
+print(Solution().Sum_Solution(10))
