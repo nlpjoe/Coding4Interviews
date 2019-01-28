1c7bd8ca0f211d9498a01c531e964288e0bca8d4
Zhou Junzuo <joezhow777@gmail.com>
2019-01-28 14:46:48 +0800

    进位做加法

---
+++ 048-不用加减乘除做加法/code.py
@@ -0,0 +1,12 @@
+from ctypes import *
+class Solution:
+    def Add(self, num1, num2):
+        # write code here
+        while num2 != 0:
+            temp = c_int(num1 ^ num2).value  # 不带进位的相加结果
+            num2 = c_int((num1 & num2) << 1).value  # 带进位
+            num1 = temp
+        return num1
+
+print(Solution().Add(-1, 23))
+
