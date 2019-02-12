76f503e21cc7e11db88aa85ceafef4af334a986e
Zhou Junzuo <joezhow777@gmail.com>
2019-02-12 16:43:05 +0800

    新增leetcode

---
+++ leetcode/005-Longest Palindromic Substring/code.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = ''
+        n = len(s)
+        for i in range(n):
+            odd_s = self.palindromeAt(s, i, i)
+            even_s = self.palindromeAt(s, i, i+1)
+            res = max(res, odd_s, even_s, key=len)
+        return res
+
+    def palindromeAt(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l+1:r]
+
+print(Solution().longestPalindrome('caba'))
+
---
+++ leetcode/005-Longest Palindromic Substring/readme.md
@@ -0,0 +1,45 @@
+## 005-Longest Palindromic Substring
+
+### 一、题目描述
+
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+```
+Example 1:
+
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+Example 2:
+
+Input: "cbbd"
+Output: "bb"
+
+Example 3:
+
+Input: "abb"
+Output: "bb"
+```
+
+### 二、思路
+
+#### 2.1 暴力算法
+
+遍历一个字符串的所有子串时间复杂度为o(n^2)，验证一个子串是否回文时间复杂度为o(n)，总共时间o(n^3)。
+
+一个思路是：如果减少子串回文验证时间？
+
+#### 2.2 动态规划
+
+每个位置的数，可能构成长度为奇数和长度为偶数的回文。
+
+- 长度为奇数，以自己为轴向两边散开
+- 长度为偶数，以自己与后一个数为轴向两边散开
+
+
+
+
+
+
+
