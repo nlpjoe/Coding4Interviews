01f6c31bed822a4bebd5a4f23740b0dbd366e8ff
Zhou Junzuo <joezhow777@gmail.com>
2019-02-14 22:29:17 +0800

    dp413 medium

---
+++ leetcode/413-Arithmetic Slices/code.py
@@ -0,0 +1,41 @@
+class Solution1(object):
+    def numberOfArithmeticSlices(self, A):
+        n = len(A)
+        dp = [ [[0, 0] for _ in range(n)] for _ in range(n)]
+        for i in range(n-2):
+            if A[i+2] - A[i+1] == A[i+1] - A[i]:
+                dp[i][i+2] = [1, A[i+1]-A[i]]
+        for d in range(3, n):
+            for i in range(n-d):
+                if dp[i][i+d-1][0]:
+                    dp[i][i+d][0] = dp[i][i+d-1][1] == (A[i+d] - A[i+d-1])
+                    if dp[i][i+d][0]:
+                        dp[i][i+d][1] = dp[i][i+d-1][1]
+        cnt = 0
+        for col in dp:
+            for row in col:
+                if row[0]: cnt += 1
+        return cnt
+
+class Solution2(object):
+    def numberOfArithmeticSlices(self, A):
+        n = len(A)
+        dp = [[None]*n for _ in range(n)]
+        for i in range(n-2):
+            if A[i+2] - A[i+1] == A[i+1] - A[i]:
+                dp[i][i+2] = A[i+1]-A[i]
+
+        for d in range(3, n):
+            for i in range(n-d):
+                if dp[i][i+d-1] is not None:
+                    if dp[i][i+d-1] == (A[i+d] - A[i+d-1]):
+                        dp[i][i+d] = dp[i][i+d-1]
+        cnt = 0
+        for col in dp:
+            for row in col:
+                if row is not None: cnt += 1
+        return cnt
+print(Solution2().numberOfArithmeticSlices([1, 1, 1, 1]))        
+print(Solution2().numberOfArithmeticSlices([1, 2, 3, 4]))
+
+
---
+++ leetcode/413-Arithmetic Slices/readme.md
@@ -0,0 +1,48 @@
+### 题目链接
+
+https://leetcode.com/problems/arithmetic-slices/
+
+A sequence of number is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+For example, these are arithmetic sequence:
+```
+1, 3, 5, 7, 9
+7, 7, 7, 7
+3, -1, -5, -9
+```
+The following sequence is not arithmetic.
+```
+1, 1, 2, 5, 7
+```
+A zero-indexed array A consisting of N numbers is given. A slice of that array is any pair of integers (P, Q) such that 0 <= P < Q < N.
+
+A slice (P, Q) of array A is called arithmetic if the sequence:
+A[P], A[p + 1], ..., A[Q - 1], A[Q] is arithmetic. In particular, this means that P + 1 < Q.
+
+The function should return the number of arithmetic slices in the array A.
+
+
+Example:
+```
+A = [1, 2, 3, 4]
+
+return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3, 4] itself.
+```
+
+### 思路
+
+求一个数列的子数列是等差数列的数目。
+
+这题和DP647比较像。遍历所有的子串需要的复杂度是o(n^2)，判断一个子串是否是等差数列时间复杂度是o(n)。
+
+所以本题的思路是通过动态规划打表降低等差数列的判断时间为o(1)，动态规划公式如下：
+
+#### 思路1 直接思路，爆内存了
+dp[i][j][0] = True if f(j)-f(i)是等差数列                            j-i=1
+
+dp[i][j][0] = dp[i][j-1][0] and f[j]-f[j-1] == dp[i][j-1][1]       j-i>2
+if dp[i][j][0]: dp[i][j][1] = dp[i][j-1][1]
+
+#### 思路2
+
+把bool标记删减掉就好了，明天看看discuss看看别人的思路。
