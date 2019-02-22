f19535561b3b1af7b17d0430082b39d38a3d69df
Zhou Junzuo <joezhow777@gmail.com>
2019-02-22 12:30:32 +0800

    dp 392 follow up

---
+++ leetcode/392. Is Subsequence/code.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return i == len(s)
+
+print(Solution().isSubsequence(s = "abc", t = "ahbgdc"))
+print(Solution().isSubsequence(s = "axc", t = "ahbgdc"))
+        
