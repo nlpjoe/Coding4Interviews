79e7227d70e64f18e3361a3c759685fbecfb7dd0
Jzzhou <joezhow777@gmail.com>
2020-02-12 11:40:06 +0800

    Update code.py

--- 剑指offer/065-矩阵中的路径/code.py
+++ 剑指offer/065-矩阵中的路径/code.py
@@ -1,20 +1,15 @@
 # -*- coding:utf-8 -*-
 def BFS(matrix, row, col, path, visited):
-    if row < 0 or row >= len(matrix) or col < 0 or col >= len(matrix[0]) or visited[row][col]: 
+    if row < 0 or row >= len(matrix) or col < 0 or col >= len(matrix[0]) or (row, col) in visited: 
         return False
     if path[0] == matrix[row][col]:
         if len(path) == 1:
             return True
-        visited[row][col] = 1
-        if BFS(matrix, row+1, col, path[1:], visited) or \
-            BFS(matrix, row-1, col, path[1:], visited) or \
-            BFS(matrix, row, col-1, path[1:], visited) or \
-            BFS(matrix, row, col+1, path[1:], visited):
-            return True
-        return False
-    else:
-        return False
-    
+        return BFS(matrix, row+1, col, path[1:], visited| {(row, col)}) or \
+            BFS(matrix, row-1, col, path[1:], visited| {(row, col)}) or \
+            BFS(matrix, row, col-1, path[1:], visited| {(row, col)}) or \
+            BFS(matrix, row, col+1, path[1:], visited| {(row, col)})
+
 class Solution:
     def hasPath(self, matrix, rows, cols, path):
         # write code here
@@ -22,7 +17,7 @@ class Solution:
         array = [array[i*cols:(i+1)*cols] for i in range(rows)]
         for i in range(rows):
             for j in range(cols):
-                visited = [[0] * len(array[0]) for _ in range(len(array))]
+                visited = set()
                 if BFS(array, i, j, list(path), visited): 
                     return True
         return False
