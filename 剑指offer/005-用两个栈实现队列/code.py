eb127c8add3a11594bdf90b72467dd1a36cec304
Jzzhou <joezhow777@gmail.com>
2020-02-12 12:25:48 +0800

    Update code.py

--- 剑指offer/005-用两个栈实现队列/code.py
+++ 剑指offer/005-用两个栈实现队列/code.py
@@ -1,16 +1,16 @@
-class Solution:
+class CQueue(object):
+
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
 
-    def push(self, node):
-        # write code here
-        self.stack1.append(node)
-
-
-    def pop(self):
-        # return xx
+    def appendTail(self, value):
+        self.stack1.append(value)
+    
+    def deleteHead(self):
         if not self.stack2:
             while self.stack1:
                 self.stack2.append(self.stack1.pop(-1))
+        if len(self.stack2) == 0: 
+            return -1
         return self.stack2.pop(-1)
