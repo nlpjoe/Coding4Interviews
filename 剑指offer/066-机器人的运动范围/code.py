d333ba84118c127b1408454c253a7716f7b801f1
Jzzhou <joezhow777@gmail.com>
2020-02-12 11:49:15 +0800

    Update code.py

--- 剑指offer/066-机器人的运动范围/code.py
+++ 剑指offer/066-机器人的运动范围/code.py
@@ -1,3 +1,23 @@
+class Solution(object):
+    def movingCount(self, m, n, k):
+        """
+        :type m: int
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        rows = m
+        cols = n
+        threshold = k
+        array = []
+        for i in range(rows):
+            res = []
+            for j in range(cols):
+                res.append(getN(i, j))
+            array.append(res)
+        visited = [[0] * len(array[0]) for _ in range(len(array))]
+        return BFS(array, 0, 0, threshold, visited)
+
 def getN(i, j):
     res = 0
     while i:
@@ -8,29 +28,13 @@ def getN(i, j):
         j //= 10
     return res
 
-def DFS(array, i, j, threshold, visited):
+def BFS(array, i, j, threshold, visited):
     if i<0 or j<0 or i>len(array)-1 or j>len(array[0])-1 or array[i][j]>threshold or visited[i][j]:
         return 0
     res = 1
     visited[i][j] = 1
-    res += DFS(array, i+1, j, threshold, visited)
-    res += DFS(array, i-1, j, threshold, visited)
-    res += DFS(array, i, j+1, threshold, visited)
-    res += DFS(array, i, j-1, threshold, visited)
+    res += BFS(array, i+1, j, threshold, visited)
+    res += BFS(array, i-1, j, threshold, visited)
+    res += BFS(array, i, j+1, threshold, visited)
+    res += BFS(array, i, j-1, threshold, visited)
     return res
-
-class Solution:
-    def movingCount(self, threshold, rows, cols):
-        # write code here
-        array = []
-        for i in range(rows):
-            res = []
-            for j in range(cols):
-                res.append(getN(i, j))
-            array.append(res)
-        from pprint import pprint
-        pprint(array)
-        visited = [[0] * len(array[0]) for _ in range(len(array))]
-        return DFS(array, 0, 0, threshold, visited)
-
-
