eac12150db6033ea664085eaeb19d41665798ed0
Jzzhou <joezhow777@gmail.com>
2020-02-12 15:45:45 +0800

    Update code.py

--- 剑指offer/024-二叉树中和为某一值的路径/code.py
+++ 剑指offer/024-二叉树中和为某一值的路径/code.py
@@ -1,31 +1,16 @@
-
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
 ### 前序遍历，深度优先遍历dfs
-result_all = []
-array = []
-def FindPath(root, expectNumber):
-    # write code here
-    if not root: return []
-    array.append(root.val)
-    expectNumber -= root.val
-    if expectNumber == 0 and not root.left and not root.right:
-        result_all.append(array[:])
-    FindPath(root.left, expectNumber)
-    FindPath(root.right, expectNumber)
-    array.pop()
-    return result_all
+class Solution(object):
+    def __init__(self):
+        self.result_all = []
+        self.array = []
 
-
-root = TreeNode(10)
-root.left = TreeNode(5)
-root.right = TreeNode(12)
-root.left.left = TreeNode(4)
-root.left.right = TreeNode(7)
-
-print(FindPath(root, 19))
-# print(FindPath(root, 15))
+    def pathSum(self, root, expectNumber):
+        if not root: return []
+        self.array.append(root.val)
+        expectNumber -= root.val
+        if expectNumber == 0 and not root.left and not root.right:
+            self.result_all.append(self.array[:])
+        self.pathSum(root.left, expectNumber)
+        self.pathSum(root.right, expectNumber)
+        self.array.pop()
+        return self.result_all
