d0e6801f90b96389b660085c89c1c1b34579be49
Jzzhou <joezhow777@gmail.com>
2020-02-12 12:33:56 +0800

    Update code.py

--- 剑指offer/008-跳台阶/code.py
+++ 剑指offer/008-跳台阶/code.py
@@ -1,10 +1,10 @@
-def jumpFloor(number):
-    # write code here
-    f1 = 1
-    f2 = 2
-    if number == 1: return f1
-    if number == 2: return f2
-    for _ in range(number-2):
-        f2, f1 = f1+f2, f2
-    return f2
-print(jumpFloor(3))
+class Solution(object):
+    def numWays(self, n):
+        if n == 0: return 1
+        f1 = 1
+        f2 = 2
+        if n == 1: return f1
+        if n == 2: return f2
+        for _ in range(n-2):
+            f2, f1 = f1+f2, f2
+        return f2 % 1000000007
