830e6f7d15837cc2892230cca36ed8aba23507ee
Jzzhou <joezhow777@gmail.com>
2020-02-12 16:13:27 +0800

    Update solution.py

--- 剑指offer/036-两个链表的第一个公共结点/solution.py
+++ 剑指offer/036-两个链表的第一个公共结点/solution.py
@@ -1,27 +1,20 @@
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-class Solution:
-    def FindFirstCommonNode(self, pHead1, pHead2):
-        # write code here
-        p1 = pHead1
-        p2 = pHead2
-        n_p1 = 0
-        n_p2 = 0
+class Solution(object):
+    def getIntersectionNode(self, pHead1, pHead2):
+        p1, n_p1 = pHead1, 0
+        p2, n_p2 = pHead2, 0
         while p1:
             p1 = p1.next
             n_p1 += 1
         while p2:
             p2 = p2.next
             n_p2 += 1
-        if n_p1 < n_p2:
+        if n_p1 < n_p2:     # p1指向长链
             pHead1, pHead2 = pHead2, pHead1
+            n_p1, n_p2 = n_p2, n_p1
+
         for _ in range(n_p1 - n_p2):
             pHead1 = pHead1.next
-        while pHead1:
+        while pHead1 and pHead2:
             if pHead1 == pHead2:
                 return pHead1
             else:
